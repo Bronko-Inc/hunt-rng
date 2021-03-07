8020d32ad7f2085c4ed93396792a0ce0c98111df
Janik Schumacher <rndmb0t@gmail.com>
2021-03-07 17:56:48 +0100

    added forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -8,7 +9,7 @@ import { TooltipDirective } from './directives/tooltip';
 
 @NgModule({
   declarations: [AppComponent, TooltipDirective, CheckboxComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
