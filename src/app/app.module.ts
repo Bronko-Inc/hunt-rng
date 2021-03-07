549cab429ddce33b97bf8b18190042c7bc2e7d87
Janik Schumacher <rndmb0t@gmail.com>
2021-03-07 17:40:36 +0100

    added checkbox to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CheckboxComponent } from './checkbox/checkbox.component';
 import { TooltipDirective } from './directives/tooltip';
 
 @NgModule({
-  declarations: [AppComponent, TooltipDirective],
+  declarations: [AppComponent, TooltipDirective, CheckboxComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
